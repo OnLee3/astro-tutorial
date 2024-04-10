944c641796f771e8d0ff778d3fbc05a3639c77ed
이재석 <jslee01@miridih.com>
2024-04-10 14:19:54 +0100

    preact 설치

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://main--splendid-kashata-0e245a.netlify.app/",
+  integrations: [preact()]
 });
