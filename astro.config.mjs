e17248f7b8e72f0c1a61e0bad2dba58990b4d741
이재석 <jslee01@miridih.com>
2024-04-10 14:15:58 +0100

    RSS 피드 기능 추가

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://main--splendid-kashata-0e245a.netlify.app/",
+});
